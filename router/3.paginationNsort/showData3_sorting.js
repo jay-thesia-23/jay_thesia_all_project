2c493412d480850c3d9bfc2557d504281370daba
jay-thesia-23 <jay.thesia.23@gmail.com>
2023-04-10 11:55:56 +0530

    #23DEV064 sort pagination next and prev button

--- router/3.paginationNsort/showData3_sorting.js
+++ router/3.paginationNsort/showData3_sorting.js
@@ -1,115 +1,145 @@
 //this file is use for pagination in a table data with sorting
 
-import mysql2 from "mysql2";
-import express from "express";
-import { authentication } from "../../autheticate/auth.js";
-import bodyParser from "body-parser";
+ import mysql2 from "mysql2";
+ import express from "express";
+ import { authentication } from "../../autheticate/auth.js";
+ import util from "util";
+ const app = express();
+ 
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ 
+ var PORT = 5000;
+ app.set("view engine", "ejs");
+ 
+ //db connection
+ var connection = mysql2.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "root",
+   database: "student_details",
+ });
+ 
+ var query = util.promisify(connection.query).bind(connection);
+ //show query
+ app.get("/show", authentication, async function (req, res) {
+   
+ 
+   let id = Number(req.query.id) || 1;
+   let col_name = req.query.col_name || "StudentID";
+   let prev_order = "asc";
+ 
+   let order = req.query.order || "asc";
+ 
+   let limit = 10;
+   let startingIndex = (id - 1) * limit || 0;
+ 
+   let start = Number(req.query.start) || 1;
+   let end = Number(req.query.end) || 10;
+   let staticStart=start
+   let staticEnd=end
 
-const app = express();
+   let nextBtn = req.query.next;
+   let prevBtn=req.query.prev
+ 
+   let totalSql = `select count(*) as pages from stu_express;`;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+   console.log(start,end,"start end i js");
+ 
+   let totalPages = await query(totalSql);
+   totalPages = Math.ceil(totalPages[0].pages / limit);
+ 
 
-var PORT = 5000;
-app.set("view engine", "ejs");
+   console.log(nextBtn,req.query.start,"next and start");
+   if (nextBtn != undefined && (req.query.start != undefined || start != 1)) {
 
-//db connection
-var connection = mysql2.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "student_details",
-});
+    console.log("inside the next btn if ");
+     if (end >= totalPages) end = totalPages;
+     else {
+       start += 10;
+       end += 10;
+       id += 10;
+     }
+   } 
+   
+   if(prevBtn!=undefined){
+     if (start <= 10) {
+       start = 1;
+     } else {
+       start -= 10;
+       id -= 10;
+       end -= 10;
+     }
+   }
+   console.log(start,end,"start end i js AFTER change");
 
-//show query
-app.get("/show", authentication, function (req, res) {
-  connection.connect();
+   staticStart=start
+   staticEnd=end
 
-  let id = req.query.id || 1;
-  let col_name = req.query.col_name || "StudentID";
-  let prev_order = "asc";
+   console.log(staticEnd,"static end");
+   
+   startingIndex = (id - 1) * limit || 0;
+console.log(startingIndex,"startin g index of the after"); 
 
-  let order = req.query.order || "asc";
-
-  let limit = 5;
-  let offset = (id - 1) * limit || 0;
-
-  let resu2;
-
-  let search = req.query.search || " ";
-
-  connection.query(
-    `select * from stu_express order by ${col_name} ${order} limit ${offset},${limit};`,
-    function (err, resu) {
-      if (err) throw err;
-      resu2 = resu;
-    }
-  );
-
-  connection.query(
-    `select count(*) as countTot from stu_express;`,
-    function (err, count_arr) {
-      let total_page = Math.ceil(count_arr[0].countTot / limit);
-
-      console.log(order, "orderrrrr111111");
-      if (order == "asc") {
-        console.log(order, "orderrrrr");
-        0;
-        prev_order = "desc";
-      } else {
-        prev_order = "asc";
-      }
-
-      // console.log(resu2);
-      res.render("data3.ejs", {
-        resu2,
-        count_arr: total_page,
-        page: id,
-        col_name,
-        prev_order,
-        order,
-      });
-    }
-  );
-});
-
-app.post("/next", (req, res) => {
-  console.log(req.body);
-
-  let idstarting = Number(req.body.idstarting) || 1;
-  let idending=Number(req.body.idending) || 10
-
-
-  if (idending<=1500) {
-    idstarting=idstarting+10
-    idending=idending+10
-    res.json({"start":`${idstarting}`,"end":`${idending}`});
-  }else{
-    console.log("can't go next");
-  }
-
-});
-
-app.post("/prev", (req, res) => {
-  console.log(req.body);
-
-  let idstarting = Number(req.body.idstarting) || 1;
-  let idending=Number(req.body.idending) || 10
-
-
-  if (idending>10) {
-    idstarting=idstarting+10
-    idending=idending+10
-    res.json({"start":`${idstarting}`,"end":`${idending}`});
-  }else{
-    console.log("can't go previous");
-  }
-
-
-});
-// app.listen(PORT, function (err) {
-//   if (err) console.log("Error in server setup");
-//   console.log("Server listening on Port", PORT);
-// });
-
-export default app;
+   let limitSql=`select * from stu_express order by ${col_name} ${order} limit ${startingIndex},${limit};`
+   let resultLimit=await query(limitSql)
+   console.log(staticEnd,"static end");
+ 
+   if (order == "asc") {
+     prev_order = "desc";
+   } else {
+     prev_order = "asc";
+   }
+ 
+   
+   res.render("data3.ejs", {
+     resu2:resultLimit,
+     count_arr: totalPages,
+     page: id,
+     col_name,
+     prev_order,
+     order,
+     start,
+     end,
+     staticStart,
+     staticEnd
+   });
+   
+  
+ });
+ 
+ // app.post("/next", (req, res) => {
+ //   console.log(req.body);
+ 
+ //   let idstarting = Number(req.body.idstarting) || 1;
+ //   let idending = Number(req.body.idending) || 10;
+ 
+ //   if (idending <= 1500) {
+ //     idstarting = idstarting + 10;
+ //     idending = idending + 10;
+ //     res.json({ start: `${idstarting}`, end: `${idending}` });
+ //   } else {
+ //     console.log("can't go next");
+ //   }
+ // });
+ 
+//  app.post("/prev", (req, res) => {
+//    console.log(req.body);
+ 
+//    let idstarting = Number(req.body.idstarting) || 1;
+//    let idending = Number(req.body.idending) || 10;
+ 
+//    if (idending > 10) {
+//      idstarting = idstarting + 10;
+//      idending = idending + 10;
+//      res.json({ start: `${idstarting}`, end: `${idending}` });
+//    } else {
+//      console.log("can't go previous");
+//    }
+//  });
+ // app.listen(PORT, function (err) {
+ //   if (err) console.log("Error in server setup");
+ //   console.log("Server listening on Port", PORT);
+ // });
+ 
+ export default app;
